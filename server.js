cea4463ebf05b0f0316d418daf21de70228340ef
nishadpnktdi <nishad.pnktdi@gmail.com>
2019-12-03 22:05:35 +0530

    add port

--- server.js
+++ server.js
@@ -107,6 +107,6 @@ app.put('/image', (req, res) => {
     .catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.listen(3000, ()=> {
-    console.log('Server running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`Server running on port 3000 ${process.env.PORT}`);
 })
